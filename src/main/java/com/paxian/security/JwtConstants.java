92f75236c8bd27d9e5550d741b81914123012f99
Pius <ppax1706@gmail.com>
2021-03-03 09:27:13 +0100

    initial commit

--- src/main/java/com/paxian/security/JwtConstants.java
+++ src/main/java/com/paxian/security/JwtConstants.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 
 public class JwtConstants {
     public static final String SECRET = "mySecreteKey";
-    public static final int EXPIRATION_TIME = 900000;
+    public static final int EXPIRATION_TIME = 3600000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER = "Authorization";
     public static final Logger LOGGER = LoggerFactory.getLogger(JwtConstants.class);
